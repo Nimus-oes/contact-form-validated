d43b730b592320a8e1878551c0cb20251472b888
Nimus-oes <sseo.code@gmail.com>
2026-09-01 16:07:59 +0900

    link labels to each input element (#1)

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,39 +1,43 @@
+import styles from "./ContactForm.module.css";
+
 export default function ContactForm() {
   return (
     <div>
       <form action="">
-        <div>
-          <label htmlFor="">Name</label>
-          <input type="text" placeholder="Your name" />
+        <div className={styles.inputItem}>
+          <label htmlFor="name">Name</label>
+          <input id="name" type="text" placeholder="Your name" />
         </div>
-        <div>
-          <label htmlFor="">Email</label>
-          <input type="email" placeholder="you@example.com" />
+        <div className={styles.inputItem}>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" placeholder="you@example.com" />
         </div>
-        <div>
-          <label htmlFor="">Phone (Optional)</label>
-          <input type="text" placeholder="010-1234-5678" />
+        <div className={styles.inputItem}>
+          <label htmlFor="phone">Phone (Optional)</label>
+          <input id="phone" type="text" placeholder="010-1234-5678" />
         </div>
-        <div>
-          <label htmlFor="">Subject</label>
-          <select name="" id="">
+        <div className={styles.inputItem}>
+          <label htmlFor="subject">Subject</label>
+          <select name="" id="subject">
             <option value="">Select a subject</option>
             <option value="general-inquiry">General inquiry</option>
             <option value="project-inquiry">Project inquiry</option>
             <option value="other">Other</option>
           </select>
         </div>
-        <div>
-          <label htmlFor="">Message</label>
+        <div className={styles.inputItem}>
+          <label htmlFor="message">Message</label>
           <textarea
             name=""
-            id=""
+            id="message"
             placeholder="Tell us about your project or inquiry..."
           ></textarea>
         </div>
-        <div>
-          <input type="text" />
-          <label htmlFor=""></label>
+        <div className={styles.checkItem}>
+          <input id="contact-consent" type="checkbox" />
+          <label htmlFor="contact-consent">
+            I consent to being contacted by the team
+          </label>
         </div>
         <div>
           <button type="submit">Send Message</button>
