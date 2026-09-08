219599899429e71ed2b4e271f163cbd29595a307
Nimus-oes <sseo.code@gmail.com>
2026-09-08 14:39:24 +0900

    register native html inputs to RHF (#4)

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,21 +1,41 @@
+import type { ContactFormValues } from "../models";
 import styles from "./ContactForm.module.css";
 import SubjectSelect from "./SubjectSelect";
+import { useForm } from "react-hook-form";
 
 export default function ContactForm() {
+  const { register, handleSubmit } = useForm<ContactFormValues>();
+  const handleFormData = (data: ContactFormValues) => console.log(data);
+
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleSubmit(handleFormData)}>
         <div className={styles.inputItem}>
           <label htmlFor="name">Name</label>
-          <input id="name" type="text" placeholder="Your name" />
+          <input
+            id="name"
+            type="text"
+            placeholder="Your name"
+            {...register("name")}
+          />
         </div>
         <div className={styles.inputItem}>
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" placeholder="you@example.com" />
+          <input
+            id="email"
+            type="email"
+            placeholder="you@example.com"
+            {...register("email")}
+          />
         </div>
         <div className={styles.inputItem}>
           <label htmlFor="phone">Phone (Optional)</label>
-          <input id="phone" type="text" placeholder="010-1234-5678" />
+          <input
+            id="phone"
+            type="text"
+            placeholder="010-1234-5678"
+            {...register("phone")}
+          />
         </div>
         <div className={styles.inputItem}>
           <label htmlFor="subject">Subject</label>
@@ -24,13 +44,17 @@ export default function ContactForm() {
         <div className={styles.inputItem}>
           <label htmlFor="message">Message</label>
           <textarea
-            name=""
             id="message"
             placeholder="Tell us about your project or inquiry..."
+            {...register("message")}
           ></textarea>
         </div>
         <div className={styles.checkItem}>
-          <input id="contact-consent" type="checkbox" />
+          <input
+            id="contact-consent"
+            type="checkbox"
+            {...register("contactConsent")}
+          />
           <label htmlFor="contact-consent">
             I consent to being contacted by the team
           </label>
